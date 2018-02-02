28d13d3af6e9a3875d6c590a913a7ecf897a2822
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-02-02 13:59:19 +0530

    Pr.C

---
+++ Pr.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+clrscr();
+printf("Enter the letter");
+scanf("%c",&a);
+if (b=='a'!!b=='e'!!b=='i'!!b=='o'!!b=='u')
+printf("The given letter is a vowel letter");
+else
+printf("The given letter is not a vowel letter");
+getch();
+return 0;
+}
